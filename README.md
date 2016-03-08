88fdf28e20b40a179ee1e28f685fccdab3a07316
cntanglijun <869058216@qq.com>
2016-03-08 16:07:15 +0800

    :boom: public jade2amd@1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# jade2amd
+
+[![Build Status](https://travis-ci.org/cntanglijun/jade2amd.svg?branch=master)](https://travis-ci.org/cntanglijun/jade2amd)
+
+> Compile jade template to amd module
+
+## Installation
+
+```shell
+npm i jade2amd -g
+```
+
+## Usage
+
+```shell
+jade2amd -p <path> -o <dir>
+```
+
+### -p <path>
+
+**&minus;p** specify the path of file|dir which we want to watch
+
+### -o <dir>
+
+**&minus;o** is optional, we can output file to the specified dir
+
+## License
+
+**MIT**
---
+++ bin/jade2amd.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const program = require('commander');
+const fs = require('fs-extra');
+const fsp = require('fs-promise');
+const vfs = require('vinyl-fs');
+const colors = require('colors');
+const jade = require('gulp-jade');
+const wrapAmd = require('gulp-wrap-amd');
+const map = require('map-stream');
+const pkg = require('../package.json');
+
+program
+  .version(pkg.version)
+  .option('-p, --path <path>', 'specify the template path')
+  .option('-o, --out <dir>', 'specify the output path')
+  .parse(process.argv);
+
+const filePath = program.path;
+const outPath = program.out;
+
+function compile(path, to) {
+  vfs.src(path)
+    .pipe(jade({
+      client: true
+    }))
+    .pipe(wrapAmd({
+      deps: ['jade'],
+      params: ['jade']
+    }))
+    .pipe(vfs.dest(to))
+    .pipe(map(file => {
+      console.log('compiled: '.cyan + file.path.magenta);
+    }));
+}
+
+function watcher(handle) {
+  fs.watch(filePath, {persistent: true, recursive: true}, handle);
+}
+
+function watcherHandle(file) {
+  if (!path.extname(file).match(/\.jade/)) {
+    return;
+  }
+
+  fsp.access(file, fs.F_OK)
+    .then(() => {
+      compile(file, outPath || path.dirname(file));
+    });
+}
+
+// filePath maybe dir|file
+fsp.stat(filePath)
+  .then(stats => {
+    if (stats.isFile()) {
+      watcher((event, file) => {
+        watcherHandle(file);
+      });
+
+    }
+    if (stats.isDirectory()) {
+      watcher((event, file) => {
+        watcherHandle(path.join(filePath, file));
+      });
+    }
+  })
+  .catch(err => {
+    console.log(err.message.red);
+  });
+
